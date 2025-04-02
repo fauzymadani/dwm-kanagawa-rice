a28c556f3478508dd2055b41757287b43a537d80
fauzymadani <153879220+fauzymadani@users.noreply.github.com>
2025-04-02 18:26:12 +0700

    optimizing the script by just running some module just 1 times

--- dwm-6.5/bar.sh
+++ dwm-6.5/bar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # detect the display e.g (:1, :2)
-export DISPLAY=$(echo $DISPLAY)
+export DISPLAY=$(echo $DISPLAY) # This prevent from [xsetroot: failed to open display e.g :0 :01]
 
 # Icon
 wifi_icon=""  # WiFi
@@ -11,31 +11,22 @@ cpu_icon="󰻠"  # CPU
 clock_icon="󰥔"  # Clock
 download_icon=""  # Download
 upload_icon=""  # Upload
-apt_upgrades_icon="󰏔"
+apt_upgrades_icon="󰏔"  # Update package icon
 
 net_interface="wlp2s0"
-
-get_net_speed() {
-    rx_bytes_old=$(cat /sys/class/net/$net_interface/statistics/rx_bytes)
-    tx_bytes_old=$(cat /sys/class/net/$net_interface/statistics/tx_bytes)
-
-    sleep 1  # wait
-
-    rx_bytes_new=$(cat /sys/class/net/$net_interface/statistics/rx_bytes)
-    tx_bytes_new=$(cat /sys/class/net/$net_interface/statistics/tx_bytes)
-
-    rx_speed=$(( (rx_bytes_new - rx_bytes_old) / 1024 )) # KB/s
-    tx_speed=$(( (tx_bytes_new - tx_bytes_old) / 1024 )) # KB/s
-
-    # make sure the value is not negative
-    [ "$rx_speed" -lt 0 ] && rx_speed=0
-    [ "$tx_speed" -lt 0 ] && tx_speed=0
-}
 
 get_apt_upgrades() {
     count=$(apt list --upgradable 2>/dev/null | grep -c '^')
     echo "$count"
 }
+
+get_datetime() {
+    echo "$(date "+%d-%m-%Y %H:%M")"
+}
+
+# Just run 1 times
+apt_upgrades="${apt_upgrades_icon} $(get_apt_upgrades) updates"
+datetime="${clock_icon} $(get_datetime)"
 
 while true; do
     # Check if the display server is active
@@ -60,16 +51,21 @@ while true; do
     cpu_usage=$(top -bn1 | awk '/^%Cpu/ {print $2 + $4}')
     cpu="${cpu_icon} ${cpu_usage}%"
 
-    # Get datetime
-    datetime="${clock_icon} $(date "+%d-%m-%Y %H:%M")"
-
-    get_net_speed
+    # Get network speed
+    rx_bytes_old=$(cat /sys/class/net/$net_interface/statistics/rx_bytes)
+    tx_bytes_old=$(cat /sys/class/net/$net_interface/statistics/tx_bytes)
+    sleep 1
+    rx_bytes_new=$(cat /sys/class/net/$net_interface/statistics/rx_bytes)
+    tx_bytes_new=$(cat /sys/class/net/$net_interface/statistics/tx_bytes)
+    rx_speed=$(( (rx_bytes_new - rx_bytes_old) / 1024 )) # KB/s
+    tx_speed=$(( (tx_bytes_new - tx_bytes_old) / 1024 )) # KB/s
+    [ "$rx_speed" -lt 0 ] && rx_speed=0
+    [ "$tx_speed" -lt 0 ] && tx_speed=0
     download_speed="${download_icon} ${rx_speed}KB/s"
     upload_speed="${upload_icon} ${tx_speed}KB/s"
-    apt_upgrades="${apt_upgrades_icon} $(get_apt_upgrades) updates"
 
     # Update status bar
     xsetroot -name " [ $wifi_status / $download_speed / $upload_speed / $apt_upgrades / $battery / $datetime ]"
 
-    sleep 2  # Update every 2 seconds
+    sleep 2  # Update setiap 2 detik
 done
